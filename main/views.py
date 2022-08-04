d801fcb1cf371a4c06b9bfa98c8cc04b36673b6f
Nimitha J <58389915+Nimitha-jagadeesha@users.noreply.github.com>
2022-08-04 18:42:39 +0530

    Update views.py

--- main/views.py
+++ main/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'main/home.html')
+
+def eyeglasses(request):
+    return render(request, 'main/eyeglasses.html')
+
+def contactlenses(request):
+    return render(request, 'main/contactlenses.html')
